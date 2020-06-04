fee078d619c71aaa357b720984d8b21c6c4d9a51
SakiKawasaki <social.ethosa@gmail.com>
2020-06-04 18:23:07 +0700

    add test8.nim

---
+++ tests/test8/main.nim
@@ -0,0 +1,29 @@
+# --- Test 8. Example for https://github.com/the-benchmarker/web-frameworks. --- #
+import akane
+
+
+proc main {.gcsafe.} =
+  let server = newServer("127.0.0.1", 5000)  # will be run at http://127.0.0.1:5000
+
+  server.pages:
+    "/":  # http://127.0.0.1:5000/
+      if request.reqMethod == HttpGet:
+        await request.answer("")
+      else:
+        await request.error("not GET :(")
+
+    "/user":  # http://127.0.0.1:5000/user
+      if request.reqMethod == HttpGet:
+        await request.answer("")
+      else:
+        await request.error("not GET :(")
+
+    regex(re"\A/user/id(\d+)\Z"):  # http://127.0.0.1:5000/user/id123456 -> {"id": 123456}
+      if request.reqMethod == HttpPost:
+        await request.sendJson(%*{"id": url[0]})
+      else:
+        await request.error("not POST :(")
+
+  server.start()
+
+main()
